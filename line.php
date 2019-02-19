82d1abdda934e152738ad0eeb65836a4beada2e1
nawatormthong <47784186+nawatormthong@users.noreply.github.com>
2019-02-19 22:18:40 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '1GBvXCw+JMaRxZTAm5ceJM6JXqAwSFzJPXlX1XANKSLT67auFFrzb2TsDHEGGrG2UY0PeqgGoZa8S8ipaGNfdlQg4MfJxxhG/ypNkoRe60nUGdpcYbqi5Gw30uSgIVeRY8PHSgaqa/3+4xEoeNd0YQdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U0a3616fe84444f3439c1328519819055',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
